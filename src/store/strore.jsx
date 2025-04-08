a0892d2e4c0adf4157222fa98d9297c33a582ff5
Nelson Mbigili <107414047+Nelsonmbigili@users.noreply.github.com>
2025-04-08 15:21:11 +0200

    Only apply middlwware in development

--- src/store/strore.jsx
+++ src/store/strore.jsx
@@ -1,23 +1,8 @@
 import { compose, legacy_createStore, applyMiddleware } from "redux";
-// import logger from "redux-logger";
+import { loggerMiddleware } from "./middleware/logger";
 import { persistStore , persistReducer} from "redux-persist";
 import storage from 'redux-persist/lib/storage'
 import { rootReducer} from "./root-reducer";
-
-
-
-
-const loggerMiddleware = (store) => (next) => (action) =>{
-    if(!action.type){
-        return next(action);
-    }
-    console.log('type: ', action.type);
-    console.log('payload: ', action.payload);
-    console.log('currentState: ', store.getState());
-    next(action);
-
-    console.log('next state: ', store.getState());
-}
 
 const persistConfig = {
     key: 'root',
@@ -27,7 +12,8 @@ const persistConfig = {
 
 const persistedReducer  = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [loggerMiddleware];
+const middleWares = [process.env.NODE_ENV === 'development' && loggerMiddleware].filter(Boolean);
+
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
